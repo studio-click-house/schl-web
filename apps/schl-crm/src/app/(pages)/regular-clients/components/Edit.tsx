05cc5aa7963ea8ee7ae394b5887a280af9273107
Israfil Miya <israfilmiya120@gmail.com>
2026-01-29 13:25:28 +0600

    chg: removed client code edit option from regular clients

--- apps/schl-crm/src/app/(pages)/regular-clients/components/Edit.tsx
+++ apps/schl-crm/src/app/(pages)/regular-clients/components/Edit.tsx
@@ -254,22 +254,6 @@ const EditButton: React.FC<PropsType> = props => {
                                     className="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2"
                                     htmlFor="grid-password"
                                 >
-                                    Client Code
-                                </label>
-                                <input
-                                    className="appearance-none block w-full bg-gray-50 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                                    name="client_code"
-                                    value={editedData.client_code || ''}
-                                    onChange={handleChange}
-                                    type="text"
-                                />
-                            </div>
-
-                            <div>
-                                <label
-                                    className="block uppercase tracking-wide text-gray-700 text-sm font-bold mb-2"
-                                    htmlFor="grid-password"
-                                >
                                     Contact Person
                                 </label>
                                 <input
