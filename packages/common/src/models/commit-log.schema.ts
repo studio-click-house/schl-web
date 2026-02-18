77ca6d25272def3bec8b69cc2e36ae07e3644836
Israfil Miya <israfilmiya120@gmail.com>
2026-02-18 17:41:40 +0600

    refactor(breaking): changed the ticket system flow

---
+++ packages/common/src/models/commit-log.schema.ts
@@ -0,0 +1,34 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Ticket } from './ticket.schema';
+
+export type CommitLogDocument = HydratedDocument<CommitLog>;
+
+@Schema({ timestamps: true })
+export class CommitLog {
+    @Prop({
+        required: true,
+        index: true,
+        ref: Ticket.name,
+        type: mongoose.Schema.Types.ObjectId,
+    })
+    ticket: mongoose.Types.ObjectId;
+
+    // github commit url
+    @Prop({ default: '', type: String })
+    url: string;
+
+    @Prop({type: String })
+    message: string;
+
+    @Prop({type: String })
+    description: string;
+
+    @Prop({ type: Date })
+    readonly createdAt: Date;
+
+    @Prop({ type: Date })
+    readonly updatedAt: Date;
+}
+
+export const CommitLogSchema = SchemaFactory.createForClass(CommitLog);
