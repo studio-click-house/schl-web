d28986766f6bdf076143de9663fb669ca5c4dc01
Israfil Miya <israfilmiya120@gmail.com>
2026-03-20 15:31:39 +0600

    fix: nextjs standalone errors fixed

--- apps/schl-portal/next.config.mjs
+++ apps/schl-portal/next.config.mjs
@@ -1,6 +1,15 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    experimental: {
+        outputFileTracingRoot: path.join(__dirname, '../../'),
+    },
     eslint: {
         ignoreDuringBuilds: true,
     },
